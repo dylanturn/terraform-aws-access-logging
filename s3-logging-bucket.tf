8f47b1fb2906eb1d249cdf1e917af64e6fecf282
Dylan Turnbull <dylanturn@gmail.com>
2022-04-15 21:33:51 -0500

    made updates to comply with provider

--- s3-logging-bucket.tf
+++ s3-logging-bucket.tf
@@ -1,45 +1,61 @@
 
 resource "aws_s3_bucket" "log_bucket" {
   bucket        = local.instance_name
-  acl           = "log-delivery-write"
   force_destroy = var.force_destroy
+
   tags = merge({
     storage_class  = "STANDARD_IA",
     retention_days = var.access_log_retention_days
   }, local.resource_tags)
 
-  dynamic "object_lock_configuration" {
-    for_each = var.enable_object_lock ? ["Enabled"] : []
-    content {
-      object_lock_enabled = object_lock_configuration.value
-      rule {
-        default_retention {
-          mode = "COMPLIANCE"
-          days = var.access_log_retention_days
-        }
-      }
+}
+
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "example" {
+  count = var.enable_object_lock ? 1 : 0
+
+  bucket = aws_s3_bucket.log_bucket.bucket
+
+  object_lock_enabled = "Enabled"
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = var.access_log_retention_days
     }
   }
+}
 
-  # Default bucket encryption on the target bucket can only be used if AES256 (SSE-S3) is selected. SSE-KMS encryption is not supported.
-  # https://docs.aws.amazon.com/AmazonS3/latest/dev/ServerLogs.html#server-access-logging-overview
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+# Default bucket encryption on the target bucket can only be used if AES256 (SSE-S3) is selected. SSE-KMS encryption is not supported.
+# https://docs.aws.amazon.com/AmazonS3/latest/dev/ServerLogs.html#server-access-logging-overview
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket_encryption" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
 
-  lifecycle_rule {
-    id      = "access_logs"
-    enabled = true
+resource "aws_s3_bucket_lifecycle_configuration" "log_bucket_lifecycle" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+
+  rule {
+    id = "access_logs"
+
     transition {
       days          = var.access_log_transition_days
       storage_class = "STANDARD_IA"
     }
+
     expiration {
       days = (var.access_log_retention_days + 1)
     }
+
+    status = "Enabled"
   }
 }
